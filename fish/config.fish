4f7f5d9a376b0d5e68d6bb02e1edb25b86513643
DiegoStafa <stafadeigo18@gmail.com>
2020-10-31 12:20:43 +0100

    fish, picom, polybar

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+export PATH=/bin:/home/diego/scripts
+
+set EDITOR nvim
+set VISUAL code
+
+alias ls="exa" 
+alias la="exa -a"
+alias lt="exa -T"
+alias ll="exa -l"
+
+alias df="df -h"
+alias du="du -h"
+
+alias colorpicker="colorpicker 2> /dev/null"
+alias yt-a="youtube-dlc -f bestaudio --no-playlist (xclip -o)"
+
+alias pdf="zathura"
+alias bt="sudo bluetoothctl"
+
+alias stream="streamlink -p mpv (xclip -o)"
--- nvim/init.vim
+++ nvim/init.vim
@@ -5,6 +5,9 @@ set wrap!
 set foldmarker={,}
 set foldmethod=marker
 set clipboard+=unnamedplus
+set tabstop=4
 
+hi VertSplit ctermbg=white ctermfg=black
+let g:NERDTreeWinSize=20
 let g:airline_theme='base16_gruvbox_dark_hard'
 nmap <C-b> :NERDTreeToggle<CR>
